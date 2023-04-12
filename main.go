c1a6064fc3a7621e54185c3188ed257b82384dcc
Ras Pi <information@com1software.com>
2023-04-12 15:50:26 -0400

    update

--- main.go
+++ main.go
@@ -4,22 +4,42 @@ import (
 	"fmt"
 	"os"
 	"runtime"
+
+	"github.com/xuri/excelize/v2"
 )
 
 func main() {
-	fmt.Printf("xlsx Tool\n")
-	fmt.Printf("Operating System : %s\n", runtime.GOOS)
+	fmt.Printf("\n xlsx Tool - Operating System : %s\n", runtime.GOOS)
 	//	args := os.Args
 	switch {
 	//-------------------------------------------------------------
 	case len(os.Args) == 2:
-		fmt.Printf("cmd %s\n", os.Args[1])
-		fmt.Println("Not")
+		cmd := os.Args[1]
+		fmt.Printf("cmd %s\n", cmd)
+		f, err := excelize.OpenFile("Book1.xlsx")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer func() {
+			// Close the spreadsheet.
+			if err := f.Close(); err != nil {
+				fmt.Println(err)
+			}
+		}()
+		// Get value from cell by given worksheet name and cell reference.
+		cell, err := f.GetCellValue("Sheet1", "B2")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(cell)
 
 		//}
 		//-------------------------------------------------------------
 	default:
-		fmt.Printf("Command %s\n", "test")
+
+		fmt.Printf("\n Syntax : %s <file> \n\n", os.Args[0])
 
 	}
 }
